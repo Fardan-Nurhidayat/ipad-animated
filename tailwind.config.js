8623a00e473bcd55cf177e77e28867abeb40e5de
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-01-12 23:49:35 +0700

    Feat : Menambahkan struktur dan style dasar di navigasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [".html"],
   theme: {
     extend: {
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        background: "#FAFAFC",
       },
     },
   },
